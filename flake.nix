cd96c24ad055a3d569e6505b4389a5a60c0524d4
Hari-c137 <hariprasadaji465@gmail.com>
2025-10-08 20:22:13 +0530

    so that works..

--- flake.nix
+++ flake.nix
@@ -5,11 +5,14 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+  outputs = { self, nixpkgs, ... } @ inputs:
+  {
+	nixosConfigurations.skynet = nixpkgs.lib.nixosSystem {
+	  modules = [
+	  	./configuration.nix
+	  ];
+	   
+	};
 
   };
 }
