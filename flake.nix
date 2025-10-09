45ebabd0d8660e1d88255e9ecc2d3f25866d0822
ClearlyOpaque <hariprasadaji465@gmail.com>
2025-10-09 23:37:02 +0530

    added nyx flake

--- flake.nix
+++ flake.nix
@@ -1,16 +1,18 @@
 {
-  description = "A very basic flake";
+  description = "Echelon";
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs, ... } @ inputs:
+  outputs = { self, nixpkgs, chaotic, ... } @ inputs:
   {
 	nixosConfigurations.skynet = nixpkgs.lib.nixosSystem {
 	  specialArgs = { inherit inputs; };
 	  modules = [
 	  	./configuration.nix
+		chaotic.nixosModules.default
 	  ];
 	   
 	};
