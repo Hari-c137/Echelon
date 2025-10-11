ae7685a94c8864b45bec3d6e3adcfe0f16ec059e
ClearlyOpaque <hariprasadaji465@gmail.com>
2025-10-11 19:15:43 +0530

    added neovim-nightly-overlay

--- flake.nix
+++ flake.nix
@@ -4,13 +4,14 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, chaotic, rust-overlay, ... } @ inputs:
+  outputs = { self, nixpkgs, chaotic, rust-overlay, neovim-nightly-overlay, ... } @ inputs:
   {
 	nixosConfigurations.skynet = nixpkgs.lib.nixosSystem {
 	  specialArgs = { inherit inputs; };
