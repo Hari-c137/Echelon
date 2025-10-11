7908249f3eb4be2611176f255c5dcd3022d1a581
ClearlyOpaque <hariprasadaji465@gmail.com>
2025-10-11 18:35:55 +0530

    added rust-overlays & updated flake

--- flake.nix
+++ flake.nix
@@ -4,17 +4,25 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, chaotic, ... } @ inputs:
+  outputs = { self, nixpkgs, chaotic, rust-overlay, ... } @ inputs:
   {
 	nixosConfigurations.skynet = nixpkgs.lib.nixosSystem {
 	  specialArgs = { inherit inputs; };
+	  system = "x86_64-linux";
 	  modules = [
 	  	./configuration.nix
 		chaotic.nixosModules.default
+		({pkgs, ... }: {
+			nixpkgs.overlays = [ rust-overlay.overlays.default ];
+			environment.systemPackages = [ pkgs.rust-bin.stable.latest.default ];
+		})
 	  ];
-	   
 	};
 
   };
