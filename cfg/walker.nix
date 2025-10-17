d21e06fc5cfd32d74de0c725aa4296eb1def6622
ClearlyOpaque <hariprasadaji465@gmail.com>
2025-10-17 21:45:08 +0530

    initial rewrite of walker.nix home-manager config

--- cfg/walker.nix
+++ cfg/walker.nix
@@ -5,6 +5,19 @@
   imports = [ inputs.walker.homeManagerModules.default ];
   programs.walker = {
     enable = true;
+    runAsService = true;
+
+    config = {
+    	click_to_close = true;
+	close_when_open = true;
+	debug = false;
+	disable_mouse = false;
+	exact_search_prefix  = "'";
+	force_keyboard_focus = false;
+	global_argument_delimiter = "#";
+	selection_wrap = false;
+	theme = "default";
+    };
   };
 
 }
