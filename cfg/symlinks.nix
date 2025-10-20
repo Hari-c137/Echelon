d6ebd09b018be6aeeeab3d8e71270c6d83d3d2b8
ClearlyOpaque <hariprasadaji465@gmail.com>
2025-10-20 16:11:28 +0530

    added mpv config

--- cfg/symlinks.nix
+++ cfg/symlinks.nix
@@ -13,4 +13,8 @@
     source = config.lib.file.mkOutOfStoreSymlink "/home/x137/code/echelon/symlinks/ewwii/";
     recursive = true;
   };
+  xdg.configFile."mpv" = {
+    source = config.lib.file.mkOutOfStoreSymlink "/home/x137/code/echelon/symlinks/mpv/";
+    recursive = true;
+  };
 }
