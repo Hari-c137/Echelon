a058e59409e32fb23d5b9d8abcb39dd06913e340
ClearlyOpaque <hariprasadaji465@gmail.com>
2025-10-28 15:36:26 +0530

    .

--- mods/nh.nix
+++ mods/nh.nix
@@ -3,8 +3,5 @@
 {
   programs.nh = {
     enable = true;
-    clean.enable = true;
-    #    clean.extraArgs = "--keep-since 4d --keep 3";
-    flake = "/home/x137/code/echelon"; # sets NH_OS_FLAKE variable for you
   };
 }
