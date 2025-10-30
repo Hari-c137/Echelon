065eba04fc0f381e38f7dbb5a3086c35dbbf156d
ClearlyOpaque <hariprasadaji465@gmail.com>
2025-10-30 15:45:51 +0530

    reformatted input & output of flake.nix

--- flake.nix
+++ flake.nix
@@ -1,37 +1,6 @@
 {
   description = "Echelon";
 
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-    wezterm = {
-      url = "github:wezterm/wezterm?dir=nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    home-manager = {
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    vicinae.url = "github:vicinaehq/vicinae";
-    niri = {
-      url = "github:sodiboo/niri-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    cosmic-ext-extra-sessions = {
-      url = "github:KiaraGrouwstra/cosmic-ext-extra-sessions?ref=niri-screen-share";
-      flake = false;
-    };
-  };
-
   outputs =
     {
       self,
@@ -86,4 +55,35 @@
         ];
       };
     };
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    wezterm = {
+      url = "github:wezterm/wezterm?dir=nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    vicinae.url = "github:vicinaehq/vicinae";
+    niri = {
+      url = "github:sodiboo/niri-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    cosmic-ext-extra-sessions = {
+      url = "github:KiaraGrouwstra/cosmic-ext-extra-sessions?ref=niri-screen-share";
+      flake = false;
+    };
+  };
 }
