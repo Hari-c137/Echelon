4c05a91b2f30cd0bf78f2a167f62bf2372a2b313
ClearlyOpaque <hariprasadaji465@gmail.com>
2025-11-01 05:06:44 +0530

    set emacs font and colorscheme

--- symlinks/doom/config.el
+++ symlinks/doom/config.el
@@ -18,7 +18,7 @@
 ;; - `doom-symbol-font' -- for symbols
 ;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 ;;
-  (setq doom-font (font-spec :family "Iosevka Nerd Font" :size 14))
+  (setq doom-font (font-spec :family "Iosevka Nerd Font" :size 17))
 
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
@@ -34,7 +34,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-ayu-dark)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
