e01e54546fa4041001a8353e1d25f0a8e31737c5
ClearlyOpaque <hariprasadaji465@gmail.com>
2025-11-01 04:39:29 +0530

    updated flake.lock (for the vicinae v0.16.0)

---
+++ symlinks/doom/custom.el
@@ -0,0 +1,19 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("0d2c5679b6d087686dcfd4d7e57ed8e8aedcccc7f1a478cd69704c02e4ee36fe"
+     "9b9d7a851a8e26f294e778e02c8df25c8a3b15170e6f9fd6965ac5f2544ef2a9" default))
+ '(package-selected-packages '(elcord vterm)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(require 'elcord)
+(elcord-mode)
+(global-wakatime-mode)
