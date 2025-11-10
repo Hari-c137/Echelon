3b3a97b1f7d5ce98d4e7e91346c921bc6df3036a
ClearlyOpaque <hariprasadaji465@gmail.com>
2025-11-10 20:20:45 +0530

    trying to fix nyx cachyos-kernel errors

--- modules/nixos/chaotic/cachyos.nix
+++ modules/nixos/chaotic/cachyos.nix
@@ -1,10 +1,24 @@
-{ inputs, pkgs, ... }:
+{
+  inputs,
+  pkgs,
+  outputs,
+  ...
+}:
+
+let
+  cachykernel = pkgs.linuxPackages_cachyos.kernel;
+in
 
 {
-  imports = [ inputs.chaotic.nixModules.default ];
+  nixpkgs.overlays = [
+    outputs.overlays.unstable-packages
+    inputs.chaotic.overlays.default
+  ];
+  chaotic.nyx.cache.enable = true;
+  imports = [ inputs.chaotic.nixosModules.default ];
 
   boot.kernelPackages = pkgs.linuxPackages_cachyos;
-
+  #system.modulesTree = [ (lib.getOutput "modules" cachykernel) ];
   services.ananicy = {
     package = pkgs.ananicy-cpp;
     rulesProvider = pkgs.ananicy-rules-cachyos_git;
--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -1,32 +1,49 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.initrd.availableKernelModules = [ "vmd" "xhci_pci" "ahci" "nvme" "usbhid" ];
+  boot.initrd.availableKernelModules = [
+    "vmd"
+    "xhci_pci"
+    "ahci"
+    "nvme"
+    "usbhid"
+    "usb_storage"
+    "sd_mod"
+  ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/a6c2e0a3-f6fe-433f-9912-a244f920dcb0";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/a6c2e0a3-f6fe-433f-9912-a244f920dcb0";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/3B85-D921";
-      fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
-    };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/1457edb2-d297-4333-92b1-f16515c935a4"; }
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/3B85-D921";
+    fsType = "vfat";
+    options = [
+      "fmask=0022"
+      "dmask=0022"
     ];
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/1457edb2-d297-4333-92b1-f16515c935a4"; }
+  ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
